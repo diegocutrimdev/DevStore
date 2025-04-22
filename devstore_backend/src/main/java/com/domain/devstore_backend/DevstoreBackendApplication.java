b0101688b7570f1597440bfc229b6e84d3d95417
dicutrim <diegocutrimcontato@gmail.com>
2025-04-22 13:33:54 -0300

    Criação do projeto e do arquivo .gitignore

---
+++ devstore_backend/src/main/java/com/domain/devstore_backend/DevstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domain.devstore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstoreBackendApplication.class, args);
+	}
+
+}
---
+++ devstore_backend/src/test/java/com/domain/devstore_backend/DevstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.devstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

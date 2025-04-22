b4b5b3f2d338574aa5be01235f0c74cd2bed665a
dicutrim <diegocutrimcontato@gmail.com>
2025-04-22 15:01:02 -0300

    Iniciando com o banco de dados e com os scripts das tabelas

--- devstore_backend/src/test/java/com/domain/devstore_backend/ApplicationTests.java
+++ devstore_backend/src/test/java/com/domain/devstore_backend/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DevstoreBackendApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
